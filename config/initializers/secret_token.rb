0ebbaa9ed17f3459df17448d70d5a99ca92480db
Victor Pereira <vpereira@deathstar.(none)>
2012-09-14 17:45:27 +0200

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BayesTest::Application.config.secret_token = '39f9284dc8983f11cd89ce48c433f8f8d535a7ec954cf33cb641d797b32dc70000ad31412fa0237e5f7ce587825769d405b02c4cfbbd3d3dca817d8e28abf193'
