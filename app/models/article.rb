4832b8aafaab5459ed0bc921d6de3b7d3c580837
Victor Pereira <vpereira@web.de>
2012-09-17 10:41:13 +0200

    index view updated

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,9 +1,15 @@
 class Article
   include Mongoid::Document
   include ActsAsBayes
-  field :title, type: String
-  field :body, type: String
-
+  field :title, type: String, :default=>""
+  field :body, type: String, :default=>""
+  field :category, type: String, :default=>""
   acts_as_bayes :on=>:title
 
+  before_save :set_category
+
+  private
+  def set_category
+    self.category = self.classify
+  end
 end
